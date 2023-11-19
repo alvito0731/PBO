fb19992eecd8c322f6cf036c9f1877e1f19928ba
alvito0731 <148175964+alvito0731@users.noreply.github.com>
2023-11-19 10:20:48 +0700

    Add files via upload

---
+++ eks2.py
@@ -0,0 +1,15 @@
+import random
+#coba coba
+Xr = random.random()
+epsilon = 0.001
+max_iter = 200
+
+for i in range(1, max_iter + 1):
+    Xrplus1 = (14 - Xr**2) / 5
+    e = abs(Xrplus1 - Xr)
+    if e < epsilon:
+        print(f'Konvergensi tercapai: gx = {Xrplus1:.6f} setelah {i} iterasi')
+        break
+    Xr = Xrplus1
+else:
+    print(f'Konvergensi tidak tercapai setelah {max_iter} iterasi')
